ce99915a6d90ce0a87c9d04de899d3929b47a124
uniQueSense <krzysztof.kurzak1@gmail.com>
2020-12-13 17:59:11 +0100

    ...

--- index.php
+++ index.php
@@ -6,5 +6,6 @@ $path = trim($_SERVER['REQUEST_URI'], '/');
 $path = parse_url( $path, PHP_URL_PATH);
 
 Router::get('', 'DefaultController');
+Router::get('index', 'DefaultController');
 Router::get('projects', 'DefaultController');
 Router::run($path);
