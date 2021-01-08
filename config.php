d901980213ae45077609479d1c039851640a7fc5
uniQueSense <krzysztof.kurzak1@gmail.com>
2021-01-08 17:27:46 +0100

    ...

--- config.php
+++ config.php
@@ -1,5 +1,1 @@
 <?php
-const USERNAME='bjidvogyjemnci';
-const PASSWORD='2dd4aea2268a41832ae29be81f18e474d53e1c34b6154d5772ecf44bd3179be7';
-const HOST='ec2-54-247-169-129.eu-west-1.compute.amazonaws.com';
-const DATABASE  ='dbb908bstv2e0o';
