58b4530185a81c34df5994de5f24817d05e08e4f
uniQueSense <krzysztof.kurzak1@gmail.com>
2021-01-11 20:59:31 +0100

    ...

--- public/views/projects.php
+++ public/views/projects.php
@@ -4,7 +4,8 @@
     <link rel="stylesheet" type="text/css" href="public/css/menu_style.css">
 
     <script src="https://kit.fontawesome.com/7186f6c2cc.js" crossorigin="anonymous"></script>
-    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Glide.js/3.4.1/glide.min.js"></script>
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Glide.js/3.4.1/css/glide.theme.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
@@ -42,14 +43,14 @@
                         <ul class="glide__slides">
 
                             <li class="glide__slide play">
-                                <img src="public/img/example_image.svg" alt="">
+                                <img src="public/img/cabaret.svg" alt="">
                                 <div class="rectangle">
                                     <h1>obraz1</h1>
                                     <a href="#">Read more!</a>
                                 </div>
 
                                 <div class="rotate">
-                                    <img src="public/img/example_image.svg" alt="">
+                                    <img src="public/img/cabaret.svg" alt="">
                                     <div class="rectangle">
                                         <h1>obraz1</h1>
                                         <a href="#">Read more!</a>
@@ -58,14 +59,46 @@
                             </li>
 
                             <li class="glide__slide play">
-                                <img src="public/img/example_image.svg" alt="">
+                                <img src="public/img/balladyna.svg" alt="">
                                 <div class="rectangle">
                                     <h1>obraz1</h1>
                                     <a href="#">Read more!</a>
                                 </div>
 
                                 <div class="rotate">
-                                    <img src="public/img/example_image.svg" alt="">
+                                    <img src="public/img/balladyna.svg" alt="">
+                                    <div class="rectangle">
+                                        <h1>obraz1</h1>
+                                        <a href="#">Read more!</a>
+                                    </div>
+                                </div>
+                            </li>
+
+                            <li class="glide__slide play">
+                                <img src="public/img/biesy.svg" alt="">
+                                <div class="rectangle">
+                                    <h1>obraz1</h1>
+                                    <a href="#">Read more!</a>
+                                </div>
+
+                                <div class="rotate">
+                                    <img src="public/img/biesy.svg" alt="">
+                                    <div class="rectangle">
+                                        <h1>obraz1</h1>
+                                        <a href="#">Read more!</a>
+                                    </div>
+                                </div>
+                            </li>
+
+                            <li class="glide__slide play">
+                                <img src="public/img/manifesto.svg" alt="">
+                                <div class="rectangle">
+                                    <h1>obraz1</h1>
+                                    <a href="#">Read more!</a>
+                                </div>
+
+                                <div class="rotate">
+                                    <img src="public/img/manifesto.svg" alt="">
                                     <div class="rectangle">
                                         <h1>obraz1</h1>
                                         <a href="#">Read more!</a>
@@ -99,20 +132,25 @@
                             <i class="fa fa-angle-right"></i>
                         </button>
                     </div>
-                </div>        
+                </div>
             </div>
         </div>
     </div>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Glide.js/3.4.1/glide.min.js"></script>
+
     <script>
-        new Glide('.glide', {
+        var input = document.querySelector('#options-focus-at-input')
+        var checkbox = document.querySelector('#options-focus-at-checkbox')
+
+        var glide = new Glide('.glide', {
             type: 'carousel',
             startAt: 0,
-            perView: 5,
-            focusAt: 'center',
-        }).mount()
-        
+            perView: 6,
+            focusAt: "center",
+        })
+
+            glide.mount()
+
     </script>
 </body>
 
