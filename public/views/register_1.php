4befc3323e8c1eba0d4eb0de548cd197b0f2532d
uniQueSense <krzysztof.kurzak1@gmail.com>
2021-01-13 16:59:27 +0100

    ...

--- public/views/register_1.php
+++ public/views/register_1.php
@@ -13,24 +13,47 @@
 </head>
 
 <body>
-<div class="container1">
-    <nav class="menu">
-        <div class="logo2">
-            <img src="public/img/logo.svg">
+    <div class="container1">
+        <nav class="menu">
+            <div class="logo2">
+                <img src="public/img/logo.svg">
 
+            </div>
+
+            <ul class="menubtns" style="float:right">
+                <li><a href="http://localhost:8080/projects">Strona Główna</a></li>
+                <li><a href="http://localhost:8080/search">Szukaj spektaklu</a></li>
+                <li><a href="http://localhost:8080/theatre">Teatry</a></li>
+                <li><a href="http://localhost:8080/aboute">O nas</a></li>
+                <li><a href="http://localhost:8080/login">Zaloguj się</a></li>
+            </ul>
+        </nav>
+
+
+        <div class="page_location"><img src="public/img/pasek2.svg"></div>
+
+        <div class="register_container">
+            <form>
+                <div class="city">
+                    <p>Miejscowość</p>
+                    <input name="city" type="text" placeholder="Warszawa">
+                </div>
+
+                <div class="street">
+                    <p>Ulica</p>
+                    <input name="street" type="text" placeholder="Wiejska">
+                </div>
+
+                <div class="home_number">
+                    <p>Numer domu / (meszkania)</p>
+                    <input name="home_number" type="text" placeholder="13">
+                </div>
+
+                <!-- <input id="rememberChkBox" type="checkbox"> -->
+                <div class="button_login">
+                    <button><i class="fas fa-arrow-right"></i></button>
+                </div>
+            </form>
         </div>
-
-        <ul class="menubtns" style="float:right">
-            <li><a href="http://localhost:8080/projects">Strona Główna</a></li>
-            <li><a href="http://localhost:8080/search">Szukaj spektaklu</a></li>
-            <li><a href="http://localhost:8080/theatre">Teatry</a></li>
-            <li><a href="http://localhost:8080/aboute">O nas</a></li>
-            <li><a href="http://localhost:8080/login">Zaloguj się</a></li>
-        </ul>
-    </nav>
-
-
-    <div class="page_location"><img src="public/img/pasek2.svg"></div>
-
-
+    </div>
 </body>
--- public/views/register_2.php
+++ public/views/register_2.php
@@ -13,24 +13,55 @@
 </head>
 
 <body>
-<div class="container1">
-    <nav class="menu">
-        <div class="logo2">
-            <img src="public/img/logo.svg">
+    <div class="container1">
+        <nav class="menu">
+            <div class="logo2">
+                <img src="public/img/logo.svg">
 
+            </div>
+
+            <ul class="menubtns" style="float:right">
+                <li><a href="http://localhost:8080/projects">Strona Główna</a></li>
+                <li><a href="http://localhost:8080/search">Szukaj spektaklu</a></li>
+                <li><a href="http://localhost:8080/theatre">Teatry</a></li>
+                <li><a href="http://localhost:8080/aboute">O nas</a></li>
+                <li><a href="http://localhost:8080/login">Zaloguj się</a></li>
+            </ul>
+        </nav>
+
+
+        <div class="page_location"><img src="public/img/pasek3.svg"></div>
+
+        <div class="register_container">
+            <form>
+                <div class="card_number">
+                    <p>Numer karty</p>
+                    <input class="inputCard" type="number" min="1000" max="9999" name="creditCard1" id="creditCard1" required/>
+                    -
+                    <input class="inputCard" type="number" min="1000" max="9999" name="creditCard2" id="creditCard2" required/>
+                    -
+                    <input class="inputCard" type="number" min="1000" max="9999" name="creditCard3" id="creditCard3" required/>
+                    -
+                    <input class="inputCard" type="number" min="1000" max="9999"  name="creditCard4" id="creditCard4" required/>
+                </div>
+
+                <div class="card_date">
+                    <p>Ważność karty</p>
+                    <input class="inputCard" name="expiry" id="expiry" type="month" required/>
+
+                </div>
+
+                <div class="cvv">
+                    <p>cvv</p>
+                    <input name="key" type="number" placeholder="xxx">
+                </div>
+
+                <!-- <input id="rememberChkBox" type="checkbox"> -->
+                <div class="button_login">
+                    <button><i class="fas fa-arrow-right"></i></button>
+                </div>
+            </form>
         </div>
 
-        <ul class="menubtns" style="float:right">
-            <li><a href="http://localhost:8080/projects">Strona Główna</a></li>
-            <li><a href="http://localhost:8080/search">Szukaj spektaklu</a></li>
-            <li><a href="http://localhost:8080/theatre">Teatry</a></li>
-            <li><a href="http://localhost:8080/aboute">O nas</a></li>
-            <li><a href="http://localhost:8080/login">Zaloguj się</a></li>
-        </ul>
-    </nav>
-
-
-    <div class="page_location"><img src="public/img/pasek3.svg"></div>
-
-
+    </div>
 </body>
