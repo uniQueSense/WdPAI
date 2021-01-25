ceb778124493418a24f0cf7a259e9313560d8e4d
uniQueSense <krzysztof.kurzak1@gmail.com>
2021-01-25 23:16:50 +0100

    new script

---
+++ public/js/spectacle-script.js
@@ -0,0 +1,57 @@
+const block1 = document.querySelector(".block-spectacle1");
+const block2 = document.querySelector(".block-spectacle2");
+const block3 = document.querySelector(".block-spectacle3");
+
+const button1 = document.querySelector(".button-cast");
+const button2 = document.querySelector(".button-description");
+const button3 = document.querySelector(".button-cost");
+
+button1.addEventListener("click",function (){
+    block1.style.display = "grid";3
+    block2.style.display = "none";
+    block3.style.display = "none";
+    button1.style.background = "blue";
+    button2.style.background = "deepskyblue";
+    button3.style.background = "deepskyblue";
+});
+
+button2.addEventListener("click",function (){
+    block1.style.display = "none";
+    block2.style.display = "grid";
+    block3.style.display = "none";
+    button1.style.background = "deepskyblue";
+    button2.style.background = "blue";
+    button3.style.background = "deepskyblue";
+});
+
+button3.addEventListener("click",function (){
+    block1.style.display = "none";
+    block2.style.display = "none";
+    block3.style.display = "grid";
+    button1.style.background = "deepskyblue";
+    button2.style.background = "deepskyblue";
+    button3.style.background = "blue";
+});
+
+const search = document.querySelector('input[placeholder="Szukaj przedstawienia"]');
+const theatreContainer = document.querySelector('.theatre_list');
+
+
+function searchHandler () {
+    const data = {'actors': search.value};
+    console.log(data)
+    fetch("/searchSearch", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }).then( response=> {
+        return response.json();
+    }).then( performances=> {
+        searchContainer.innerHTML = "";
+        loadPerformances(performances)
+    });
+};
+
+document.querySelector('.button-search').addEventListener('click', () => searchHandler())
--- public/views/spectacle.php
+++ public/views/spectacle.php
@@ -7,28 +7,27 @@ if( ! isset($spectacle)) {
 
 <head>
     <link rel="stylesheet" type="text/css" href="public/css/menu_style.css">
-    <link rel="stylesheet" type="text/css" href="public/css/register_style.css">
+    <link rel="stylesheet" type="text/css" href="public/css/style_backg.css">
 
-    <script type="text/javascript" src="./public/js/script.js" defer></script>
+    <script type="text/javascript" src="./public/js/spectacle-script.js" defer></script>
     <script src="https://kit.fontawesome.com/7186f6c2cc.js" crossorigin="anonymous"></script>
 
-    <title>Spektakl:<??></title>
+    <title>Spektakl: <?= $spectacle[0]['title']; ?></title>
 
 </head>
 
 <body>
-<div class="container1">
+<div class="container-spectacle">
     <?PHP include 'menu_component.php'?>
+
+    <h10>  <?= $spectacle[0]['title']; ?>  </h10>
 
     <div class="images-slide">
         <ul class="glide__slide">
 
             <?php
             $images = json_decode($spectacle[0]["image"], true);
-
-            foreach ($images as $image):
-            ?>
-
+            foreach ($images as $image):?>
                 <li class="glide__slide play">
                     <img class="spectacle-img" src="<?= $image['route']; ?>" alt="">
                 </li>
@@ -38,9 +37,35 @@ if( ! isset($spectacle)) {
     </div>
 
     <div class="button-navi">
-
+        <button class="button-navigation button-cast">Aktorzy</button>
+        <button class="button-navigation button-description">Opis spektaklu</button>
+        <button class="button-navigation button-cost">Bilety</button>
     </div>
 
+    <div class="block-spectacle1">
+        <h11>Obsada:</h11>
+        <div class="actors-block">
+            <ul>
+        <?php foreach ($actors as $actor): ?>
+            <li>
+                <p><?PHP echo $actor['first_name'].' '.$actor['second_name']?></p>
+            </li>
+        <?php endforeach; ?>
+            </ul>
+        </div>
+    </div>
+
+    <div class="block-spectacle2">
+        <ul>
+            <li>
+                <p><?= $spectacle[0]['description']; ?></p>
+            </li>
+        </ul>
+    </div>
+
+    <div class="block-spectacle3">
+        <p><?= $spectacle[0]['cost']?></p>
+    </div>
 
 
 </body>
