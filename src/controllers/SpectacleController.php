6e70355b39be25b7b10902042496ce836b3c2998
uniQueSense <krzysztof.kurzak1@gmail.com>
2021-01-25 13:24:16 +0100

    new function chosenSpectacle and getOnceSpectacle

--- src/controllers/SpectacleController.php
+++ src/controllers/SpectacleController.php
@@ -1,14 +1,20 @@
 <?php
 
 
-
-
 class SpectacleController extends AppController {
 
     public function recommendedSpectacle() {
         $spectacleRepository = new SpectacleRepository();
         $spectacle = $spectacleRepository->getRandomSpectacle();
+
         $this->render('projects', ['spectacle'=>$spectacle]);
+    }
+
+    public function chosenSpectacle() {
+        $spectacleRepository = new SpectacleRepository();
+        $thisSpectacle = $spectacleRepository->getOnceSpectacle(1);
+
+        $this->render('spectacle', ['spectacle'=>$thisSpectacle]);
     }
 
 
--- src/repository/SpectacleRepository.php
+++ src/repository/SpectacleRepository.php
@@ -6,7 +6,7 @@ require_once  __DIR__.'/../models/Performances.php';
 class SpectacleRepository extends Repository {
 
     public function getRandomSpectacle(): ?array {
-        $stmt =$this->database->connect()->prepare('
+        $stmt = $this->database->connect()->prepare('
         SELECT * FROM spectacle ORDER BY random() LIMIT 15
         ');
 
@@ -26,6 +26,18 @@ class SpectacleRepository extends Repository {
         }
 
         return $array;
+
+    }
+
+    public function getOnceSpectacle($int): ?array {
+
+        $stmt = $this->database->connect()->prepare('
+        SELECT * FROM spectacle WHERE id_spectacle = ?');
+
+        $stmt->execute([$int]);
+        $spectacle = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        return $spectacle;
 
     }
 
