3f123e21711e14a55c2912abf4c70777dc2b0c51
uniQueSense <krzysztof.kurzak1@gmail.com>
2021-01-26 14:58:41 +0100

    cosmetic changing

--- public/js/spectacle-script.js
+++ public/js/spectacle-script.js
@@ -27,7 +27,7 @@ button2.addEventListener("click",function (){
 button3.addEventListener("click",function (){
     block1.style.display = "none";
     block2.style.display = "none";
-    block3.style.display = "grid";
+    block3.style.display = "flex";
     button1.style.background = "deepskyblue";
     button2.style.background = "deepskyblue";
     button3.style.background = "blue";
