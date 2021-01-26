d00d632f3d8a5c55856354e28d9c5d3c382c8a09
uniQueSense <krzysztof.kurzak1@gmail.com>
2021-01-26 23:05:18 +0100

    cosmetic changing

--- public/js/spectacle-script.js
+++ public/js/spectacle-script.js
@@ -7,30 +7,39 @@ const button2 = document.querySelector(".button-description");
 const button3 = document.querySelector(".button-cost");
 
 button1.addEventListener("click",function (){
-    block1.style.display = "grid";3
+    block1.style.display = "flex";
     block2.style.display = "none";
     block3.style.display = "none";
-    button1.style.background = "blue";
-    button2.style.background = "deepskyblue";
-    button3.style.background = "deepskyblue";
+    button1.style.background = "white";
+    button1.style.color = "#2699FB"
+    button2.style.background = "#2699FB";
+    button2.style.color = "white"
+    button3.style.background = "#2699FB";
+    button3.style.color = "white"
 });
 
 button2.addEventListener("click",function (){
     block1.style.display = "none";
     block2.style.display = "grid";
     block3.style.display = "none";
-    button1.style.background = "deepskyblue";
-    button2.style.background = "blue";
-    button3.style.background = "deepskyblue";
+    button1.style.background = "#2699FB";
+    button1.style.color = "white"
+    button2.style.background = "white";
+    button2.style.color = "#2699FB"
+    button3.style.background = "#2699FB";
+    button3.style.color = "white"
 });
 
 button3.addEventListener("click",function (){
     block1.style.display = "none";
     block2.style.display = "none";
     block3.style.display = "flex";
-    button1.style.background = "deepskyblue";
-    button2.style.background = "deepskyblue";
-    button3.style.background = "blue";
+    button1.style.background = "#2699FB";
+    button1.style.color = "white"
+    button2.style.background = "#2699FB";
+    button2.style.color = "white"
+    button3.style.background = "white";
+    button3.style.color = "#2699FB"
 });
 
 const search = document.querySelector('input[placeholder="Szukaj przedstawienia"]');
