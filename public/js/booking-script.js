4cb18011b2f1b2c79fb8bae456f309cb486c4cec
uniQueSense <krzysztof.kurzak1@gmail.com>
2021-01-27 22:54:09 +0100

    create js for booking.php view

---
+++ public/js/booking-script.js
@@ -0,0 +1,41 @@
+const chairs = document.querySelectorAll('.armchair');
+const rowInput = document.querySelector('#row');
+const columnInput = document.querySelector('#column');
+const selectedColor = 'rgb(0, 255, 0)'
+const notSelectedColor = 'rgb(135, 206, 250)'
+let currentlySelected = null
+
+chairs.forEach(chair => chair.style.color = notSelectedColor);
+
+chairs.forEach( chair => chair.addEventListener('click', function() {
+    if(currentlySelected !== null) {
+        changeColor(currentlySelected);
+    }
+    currentlySelected = chair;
+    changeColor(currentlySelected);
+    writeToInput(currentlySelected);
+})
+);
+
+const changeColor =  (element) => {
+    const color = element.style.color;
+    console.log(color);
+    console.log(typeof color);
+    if(color == notSelectedColor) {
+        element.style.color = selectedColor;
+    } else {
+        element.style.color = notSelectedColor;
+    }
+}
+
+const writeToInput = (element) => {
+    const values = getRowAndCol(element.querySelector('i'));
+
+    rowInput.value = values[0];
+    columnInput.value = values[1];
+}
+
+const getRowAndCol = (element) => {
+    let id = element.id;
+    return id.split('/');
+}
