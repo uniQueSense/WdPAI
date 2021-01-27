e3d07de93641ed52b70f6b09382fa7496dc42669
uniQueSense <krzysztof.kurzak1@gmail.com>
2021-01-27 22:54:24 +0100

    create js for spectacle.php view

---
+++ public/js/glidescript-to-spectacle.js
@@ -0,0 +1,17 @@
+var input = document.querySelector('#options-focus-at-input')
+var checkbox = document.querySelector('#options-focus-at-checkbox')
+
+var glide = new Glide('.glide', {
+    type: 'carousel',
+    startAt: 1,
+    perView: 3,
+    gap: 3,
+    focusAt: "center",
+    breakpoints: {
+        800: {
+            perView: 2
+        }
+    }
+})
+
+glide.mount()
